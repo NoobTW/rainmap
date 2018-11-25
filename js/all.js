087b44d0993f6a315b50d831f6de9a76ca5825cb
NoobTW <noob@noob.tw>
2018-11-26 04:22:58 +0800

    add apis

--- js/all.js
+++ js/all.js
@@ -1,4 +1,5 @@
 var map;
+var mychart;
 
 // 宣告 map
 map = L.map('map').setView([22.9185024, 120.5786888], 9);
@@ -9,8 +10,65 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // 按下去的時候
 map.on('click', (e) => {
-	// 取得資料
-	$.getJSON('https://api-proxy.noob.tw/http://140.127.220.46:5001/api/NearWStation?location=' + e.latlng.lat + ',' + e.latlng.lng, (data) => {
-		console.log(data)
-	});
+	loadRainData(e.latlng.lat, e.latlng.lng);
 });
+
+function loadRainData(lat, lng){
+	// 取得資料
+
+	if(mychart) mychart.destroy();
+	$.getJSON('http://140.127.220.46:5002/api/NearWStation?location=' + lat + ',' + lng, (data) => {
+		if(data.length){
+			var stationName = Object.keys(data[0]);
+			var distance = Object.values(data[0])[0].slice(0, -3);
+
+			$('h2').html('來自 <span id="station">' + stationName + '</span>的觀測站資料（距離 ' + Math.floor(distance * 100) / 100 + ' km）')
+			$.getJSON('http://140.127.220.46:5002/api/RainFall?WS=' + stationName, (rainData) => {
+				rainData = rainData.filter(x => x[0].startsWith('2018'));
+
+				var rainsPerHour = [];
+				var rains = [];
+				var labels = [];
+
+				var j = 0;
+				for(let i = moment('2018-01-01 00:00:00'); i.unix() < moment().unix(); i = moment(i).add(1, 'hours')){
+					// var data = rainData.find( x => x[0] === moment(i).format('YYYY-MM-DD HH:mm:ss'));
+
+					if(j < rainData.length && moment(rainData[j][0]).unix() === moment(i).unix()){
+						rainsPerHour.push(rainData[j][1]);
+						j++;
+					}else{
+						rainsPerHour.push(0);
+					}
+
+					if(moment(i).format('HH:mm') === '23:00'){
+						labels.push(moment(i).format('YYYY-MM-DD'));
+						rains.push(rainsPerHour.reduce((x, y) => x+y));
+						rainsPerHour = [];
+					}
+				}
+
+
+				var ctx = document.getElementById("chart").getContext('2d');
+				mychart = new Chart(ctx, {
+					type: 'bar',
+					data: {
+						labels: labels,
+						datasets: [{
+							label: stationName + ' 日雨量',
+							data: rains,
+							backgroundColor: '#ff4500',
+							borderColor: '#ff4500',
+							borderWidth: 1
+						}]
+					},
+					options: {
+						maintainAspectRatio: false
+					},
+				})
+			});
+		}else{
+			$('h2').text('沒有資料');
+		}
+	});
+}
